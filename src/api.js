69f71000f60e103a40fd346435cebd86d2118d86
TKN-OPEN <120709149+TKN-OPEN@users.noreply.github.com>
2022-12-19 14:25:47 +0900

    Delete src directory

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages() {
-    const response = await fetch(
-      `https://aws.random.cat/meow`
-    );
-    const data = await response.json();
-    return data.file;
-  }
